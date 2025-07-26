ba25940ebfc4b5ab2bdb6dacd3b7b58c24079315
v.havryshkevych <v.havryshkevych@sellerlogic.com>
2025-07-26 07:28:44 +0300

    update 26 jule

--- src/downloadGuides.ts
+++ src/downloadGuides.ts
@@ -1,10 +1,83 @@
 import fs from 'fs';
+import pLimit from 'p-limit';
 import https from 'https';
 import path from 'path';
 import { chromium } from 'playwright';
 import { fetchGuideLinks } from './fetchGuides';
 import { DOWNLOAD_DIR } from './config';
 
+const CONCURRENCY_LIMIT = 10;
+
+async function downloadHero(hero: { link: any; image: any; }) {
+    const browser = await chromium.launch();
+    const context = await browser.newContext();
+
+    const cookies = JSON.parse(fs.readFileSync('cookies.json', 'utf8'));
+    await context.addCookies(cookies);
+
+    const page = await context.newPage();
+
+    console.log(`Сохраняю: ${hero.link}`);
+    await page.goto(hero.link, { waitUntil: 'networkidle' });
+    const heroSelector = '#root > div > div > main > div > div:nth-child(2) > div';
+    await page.waitForTimeout(500);
+    const block = await page.$(heroSelector); // замени на реальный селектор!
+    const box = await block?.boundingBox();
+    if (hero.image) {
+        try {
+            const heroName = hero.link.split('/').pop() || 'unknown';
+            const savePath = path.join(DOWNLOAD_DIR, heroName, 'avatar.png');
+            await downloadImage(hero.image, savePath);
+        } catch (error) {
+            console.error(`⚠️ Failed to download image for hero: ${hero.link.substring(hero.link.lastIndexOf('/')+1)}`, error);
+        }
+    } else {
+        console.warn(`⚠️ No image source found for hero: ${hero.link}`);
+    }
+
+    if (box) {
+        await page.screenshot({
+            path: DOWNLOAD_DIR + '/' + hero.link.substring(hero.link.lastIndexOf('/') + 1) + `/hero.png`,
+            clip: {
+                x: box.x,
+                y: box.y,
+                width: box.width,
+                height: box.height,
+            },
+        });
+        console.log(`✅ Скриншот героя сохранён`);
+    }
+
+    const itemsSelector = '#root > div > div > main > div > div:nth-child(3) > div';
+    const tabSelector = '.flex.flex-row.items-center.gap-[8px] > div'; // или уточнить путь
+    const tabBlock = page.locator('.flex.flex-row.items-center.gap-\\[8px\\]');
+    const tabs = tabBlock.locator('> div');
+
+    for (let i = 0; i <= 3; i++) {
+        await tabs.nth(i).click();
+        await page.waitForTimeout(500); // ждём обновление содержимого
+        const block = await page.$(itemsSelector); // замени на реальный селектор!
+        const box = await block?.boundingBox();
+        if (box) {
+            if (!fs.existsSync(DOWNLOAD_DIR + '/' + hero.link.substring(hero.link.lastIndexOf('/') + 1))) {
+                fs.mkdirSync(DOWNLOAD_DIR + '/' + hero.link.substring(hero.link.lastIndexOf('/') + 1));
+            }
+            await page.screenshot({
+                path: DOWNLOAD_DIR + '/' + hero.link.substring(hero.link.lastIndexOf('/') + 1) + `/tab_${i+1}.png`,
+                clip: {
+                    x: box.x,
+                    y: box.y,
+                    width: box.width,
+                    height: box.height,
+                },
+            });
+            console.log(`✅ Скриншот вкладки ${i+1} сохранён`);
+        } else {
+            console.warn(`⚠️ Не удалось найти блок для вкладки ${i}`);
+        }
+    }
+    await browser.close();
+}
 
 async function downloadImage(imageUrl: string, savePath: string) {
     const dir = path.dirname(savePath);
@@ -35,80 +108,18 @@ async function downloadImage(imageUrl: string, savePath: string) {
 }
 
 (async () => {
-    const browser = await chromium.launch();
-    const context = await browser.newContext();
 
-    const cookies = JSON.parse(fs.readFileSync('cookies.json', 'utf8'));
-    await context.addCookies(cookies);
-    console.log('start');
-    const page = await context.newPage();
     const links = await fetchGuideLinks();
     console.log(links)
     if (!fs.existsSync(DOWNLOAD_DIR)) {
         fs.mkdirSync(DOWNLOAD_DIR);
     }
 
-    for (const hero of links) {
-        console.log(`Сохраняю: ${hero.link}`);
-        await page.goto(hero.link, { waitUntil: 'networkidle' });
-        const heroSelector = '#root > div > div > main > div > div:nth-child(2) > div';
-        await page.waitForTimeout(500);
-        const block = await page.$(heroSelector); // замени на реальный селектор!
-        const box = await block?.boundingBox();
-        if (hero.image) {
-            try {
-                const heroName = hero.link.split('/').pop() || 'unknown';
-                const savePath = path.join(DOWNLOAD_DIR, heroName, 'avatar.png');
-                await downloadImage(hero.image, savePath);
-            } catch (error) {
-                console.error(`⚠️ Failed to download image for hero: ${hero.link.substring(hero.link.lastIndexOf('/')+1)}`, error);
-            }
-        } else {
-            console.warn(`⚠️ No image source found for hero: ${hero.link}`);
-        }
+    const limit = pLimit(CONCURRENCY_LIMIT);
+    const downloadTasks = links.map((hero) =>
+        limit(() => downloadHero(hero))
+    );
 
-        if (box) {
-            await page.screenshot({
-                path: DOWNLOAD_DIR + '/' + hero.link.substring(hero.link.lastIndexOf('/') + 1) + `/hero.png`,
-                clip: {
-                    x: box.x,
-                    y: box.y,
-                    width: box.width,
-                    height: box.height,
-                },
-            });
-            console.log(`✅ Скриншот героя сохранён`);
-        }
-
-        const itemsSelector = '#root > div > div > main > div > div:nth-child(3) > div';
-        const tabSelector = '.flex.flex-row.items-center.gap-[8px] > div'; // или уточнить путь
-        const tabBlock = page.locator('.flex.flex-row.items-center.gap-\\[8px\\]');
-        const tabs = tabBlock.locator('> div');
-
-        for (let i = 0; i <= 3; i++) {
-            await tabs.nth(i).click();
-            await page.waitForTimeout(500); // ждём обновление содержимого
-            const block = await page.$(itemsSelector); // замени на реальный селектор!
-            const box = await block?.boundingBox();
-            if (box) {
-                if (!fs.existsSync(DOWNLOAD_DIR + '/' + hero.link.substring(hero.link.lastIndexOf('/') + 1))) {
-                    fs.mkdirSync(DOWNLOAD_DIR + '/' + hero.link.substring(hero.link.lastIndexOf('/') + 1));
-                }
-                await page.screenshot({
-                    path: DOWNLOAD_DIR + '/' + hero.link.substring(hero.link.lastIndexOf('/') + 1) + `/tab_${i+1}.png`,
-                    clip: {
-                        x: box.x,
-                        y: box.y,
-                        width: box.width,
-                        height: box.height,
-                    },
-                });
-                console.log(`✅ Скриншот вкладки ${i+1} сохранён`);
-            } else {
-                console.warn(`⚠️ Не удалось найти блок для вкладки ${i}`);
-            }
-        }
-    }
-
-    await browser.close();
+    await Promise.all(downloadTasks);
+    console.log('✅ Все герои загружены');
 })();
